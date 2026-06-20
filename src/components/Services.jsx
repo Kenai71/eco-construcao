50771c3b925caa732dd274159ffd92fd856ed4d2
Kenai71 <kenaialmeida22@gmail.com>
2026-06-19 21:24:45 -0300

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,7 +14,7 @@ const services = [
   },
   {
     icon: Warehouse,
-    title: 'Cobertura Sanduíche',
+    title: 'Telha Sanduíche',
     description:
       'Instalação e manutenção de coberturas Imperlast, proporcionando alta durabilidade e estanqueidade total.',
     features: ['Alta durabilidade', 'Proteção contínua', 'Estanqueidade'],
